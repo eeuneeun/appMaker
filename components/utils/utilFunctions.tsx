315d45f13f1af84532df8974bbee3dd29ce54d8a
eeuneeun <sacroo89@gmail.com>
2023-06-21 23:51:27 +0900

    웹팩  오류 수정
    - Typescript Critical dependency 에러 유발, 사용하지 않는 함수

--- components/utils/utilFunctions.tsx
+++ components/utils/utilFunctions.tsx
@@ -1,18 +1,11 @@
 // # 타입 체크
 // @ target : 타입 체크를 원하는 객체
 
-import { useSelector } from 'react-redux';
 import { AppPageInfo, CompoInfo } from 'store/reducers/appPageSlice';
 
 // @ return : target 의 구체적 타입
 export function getType(target: any) {
   return Object.prototype.toString.call(target).slice(8, -1);
-}
-
-// # 동적 파일 임포트
-// @ url : 가져오기 원하는 파일의 경로, '' 문자를 제외해야 함
-export function DynamicImport(url: string) {
-  import(url).then((obj) => console.log(obj)).catch((err) => console.log(err));
 }
 
 // # 앱 페이지 리스트 배열 내부의 인덱스 값 얻기
